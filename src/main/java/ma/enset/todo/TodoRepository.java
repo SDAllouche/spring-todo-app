a1f3f7613198d1088ce7603506801ea180e18f69
SALAHEDDINE ALLOUCHE <sdalami123@gmail.com>
2023-05-01 12:07:39 +0100

    updating

---
+++ src/main/java/ma/enset/todo/TodoRepository.java
@@ -0,0 +1,9 @@
+package ma.enset.todo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TodoRepository extends JpaRepository<Todo, Integer>{
+	public List<Todo> findByUsername(String username);
+}
